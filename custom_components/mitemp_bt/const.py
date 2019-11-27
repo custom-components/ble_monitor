3b5560fd9eebebfda1e22c6ac6422b7ecdaa0c6a
Ernst Klamer <e.klamer@gmail.com>
2019-11-27 16:57:17 +0100

    Adding const.py

---
+++ custom_components/mitemp_bt/const.py
@@ -0,0 +1,25 @@
+"""Constants for the Xiaomi Mi temperature and humidity monitor integration."""
+
+# Configuration options
+CONF_ROUNDING = "rounding"
+CONF_DECIMALS = "decimals"
+CONF_PERIOD = "period"
+CONF_LOG_SPIKES = "log_spikes"
+CONF_USE_MEDIAN = "use_median"
+CONF_HCITOOL_ACTIVE = "hcitool_active"
+
+# Default values for configuration options
+DEFAULT_ROUNDING = True
+DEFAULT_DECIMALS = 2
+DEFAULT_PERIOD = 60
+DEFAULT_LOG_SPIKES = False
+DEFAULT_USE_MEDIAN = False
+DEFAULT_HCITOOL_ACTIVE = False
+
+"""Fixed constants."""
+
+# Sensor measurement limits to exclude erroneous spikes from the results
+CONF_TMIN = -40.0
+CONF_TMAX = 60.0
+CONF_HMIN = 0.0
+CONF_HMAX = 99.9
