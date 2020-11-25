e10d6b50339cb39a1d7c19d966d55dc03ed1adeb
Ernst Klamer <e.klamer@gmail.com>
2020-11-25 22:15:07 +0100

    Add config flow not finished yet
    Commented parts need adjustment

--- custom_components/ble_monitor/__init__.py
+++ custom_components/ble_monitor/__init__.py
@@ -5,8 +5,8 @@ import queue
 import struct
 from threading import Thread
 import voluptuous as vol
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers import discovery
+
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import (
     CONF_DEVICES,
     CONF_DISCOVERY,
@@ -15,6 +15,9 @@ from homeassistant.const import (
     CONF_TEMPERATURE_UNIT,
     EVENT_HOMEASSISTANT_STOP,
 )
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import discovery
 
 from Cryptodome.Cipher import AES
 
@@ -48,6 +51,8 @@ from .const import (
     CONF_ENCRYPTION_KEY,
     DOMAIN,
     XIAOMI_TYPE_DICT,
+    MAC_REGEX,
+    AES128KEY_REGEX,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -60,9 +65,8 @@ CND_STRUCT = struct.Struct("<H")
 ILL_STRUCT = struct.Struct("<I")
 FMDH_STRUCT = struct.Struct("<H")
 
-# regex constants for configuration schema
-MAC_REGEX = "(?i)^(?:[0-9A-F]{2}[:]){5}(?:[0-9A-F]{2})$"
-AES128KEY_REGEX = "(?i)^[A-F0-9]{32}$"
+# TO BE ADDED BINARY SENSOR
+PLATFORMS = ["sensor"]
 
 DEVICE_SCHEMA = vol.Schema(
     {
@@ -73,47 +77,68 @@ DEVICE_SCHEMA = vol.Schema(
     }
 )
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Optional(CONF_ROUNDING, default=DEFAULT_ROUNDING): cv.boolean,
-                vol.Optional(CONF_DECIMALS, default=DEFAULT_DECIMALS): cv.positive_int,
-                vol.Optional(CONF_PERIOD, default=DEFAULT_PERIOD): cv.positive_int,
-                vol.Optional(CONF_LOG_SPIKES, default=DEFAULT_LOG_SPIKES): cv.boolean,
-                vol.Optional(CONF_USE_MEDIAN, default=DEFAULT_USE_MEDIAN): cv.boolean,
-                vol.Optional(CONF_ACTIVE_SCAN, default=DEFAULT_ACTIVE_SCAN): cv.boolean,
-                vol.Optional(
-                    CONF_HCI_INTERFACE, default=[DEFAULT_HCI_INTERFACE]
-                ): vol.All(cv.ensure_list, [cv.positive_int]),
-                vol.Optional(
-                    CONF_BATT_ENTITIES, default=DEFAULT_BATT_ENTITIES
-                ): cv.boolean,
-                vol.Optional(
-                    CONF_REPORT_UNKNOWN, default=DEFAULT_REPORT_UNKNOWN
-                ): cv.boolean,
-                vol.Optional(CONF_DISCOVERY, default=DEFAULT_DISCOVERY): cv.boolean,
-                vol.Optional(CONF_RESTORE_STATE, default=DEFAULT_RESTORE_STATE): cv.boolean,
-                vol.Optional(CONF_DEVICES, default=[]): vol.All(
-                    cv.ensure_list, [DEVICE_SCHEMA]
-                ),
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
 
-
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up integration."""
+
+    if not DOMAIN in config:
+        return True
+    
+    _LOGGER.info("Initializing BLE Monitor integration")
+    hass.async_add_job(hass.config_entries.flow.async_init(
+    DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN]
+    ))
+
+    # NOT SURE ABOUT THIS PART. TEST TOMORROW
     blemonitor = BLEmonitor(config[DOMAIN])
     hass.bus.listen(EVENT_HOMEASSISTANT_STOP, blemonitor.shutdown_handler)
     blemonitor.start()
-    hass.data[DOMAIN] = blemonitor
-    discovery.load_platform(hass, "sensor", DOMAIN, {}, config)
-    discovery.load_platform(hass, "binary_sensor", DOMAIN, {}, config)
+    hass.data[DOMAIN] = blemonitor # NOT SURE HOW TO CONVERT THIS ONE
+    # discovery.load_platform(hass, "sensor", DOMAIN, {}, config) # REMOVE? WILL BE LOADED IN hass.config_entries.async_forward_entry_setup
+    # discovery.load_platform(hass, "binary_sensor", DOMAIN, {}, config) # REMOVE? WILL BE LOADED IN hass.config_entries.async_forward_entry_setup
+    # END NOT SURE ABOUT THIS PART. TEST TOMORROW
+
     return True
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up BLE Monitor from a config entry."""
+    _LOGGER.info("Initializing BLE Monitor entry")
+
+    if not entry.unique_id:
+        hass.config_entries.async_update_entry(entry, unique_id=entry.title)
+
+    if not entry.options:
+        # Move settings to options after initial setup
+        options = entry.data
+        data = {}
+        hass.config_entries.async_update_entry(entry, data=data, options=options)
+
+    entry.add_update_listener(_async_update_listener)
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    return unload_ok
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
+    
 
 class BLEmonitor:
     """BLE scanner."""
