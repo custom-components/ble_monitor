0a91f7dcb7e6211febea735a05229eaa1d7ce5db
Ernst79 <e.klamer@gmail.com>
2021-07-16 23:13:44 +0200

    More tests

---
+++ custom_components/ble_monitor/test/test_qingping_parser.py
@@ -0,0 +1,76 @@
+"""The tests for the Qingping ble_parser."""
+import unittest
+from ble_monitor.ble_parser import ble_parser
+
+
+class TestQingping(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.lpacket_ids = {}
+        cls.movements_list = {}
+        cls.adv_priority = {}
+        cls.trackerlist = []
+        cls.report_unknown = "other"
+        cls.discovery = True
+
+    def test_qingping_CGP1W(self):
+        """Test Qingping parser for CGP1W."""
+        data_string = "043E28020100006F1C40342D581C0201061816CDFD08096F1C40342D580104BE000D0207027226020157D1"
+        data = bytes(bytearray.fromhex(data_string))
+        sensor_msg, tracker_msg = ble_parser(self, data)
+
+        self.assertEqual(sensor_msg["firmware"], "Qingping")
+        self.assertEqual(sensor_msg["type"], "CGP1W")
+        self.assertEqual(sensor_msg["mac"], "582D34401C6F")
+        self.assertEqual(sensor_msg["packet"], "no packet id")
+        self.assertTrue(sensor_msg["data"])
+        self.assertEqual(sensor_msg["temperature"], 19.0)
+        self.assertEqual(sensor_msg["humidity"], 52.5)
+        self.assertEqual(sensor_msg["pressure"], 984.2)
+        self.assertEqual(sensor_msg["battery"], 87)
+        self.assertEqual(sensor_msg["rssi"], -47)
+
+    def test_qingping_CGD1(self):
+        """Test Qingping parser for CGD1."""
+        data_string = "043E2402010000BF6552342D58180201061416CDFD080CBF6552342D580104F100AD01020125D1"
+        data = bytes(bytearray.fromhex(data_string))
+        sensor_msg, tracker_msg = ble_parser(self, data)
+
+        self.assertEqual(sensor_msg["firmware"], "Qingping")
+        self.assertEqual(sensor_msg["type"], "CGD1")
+        self.assertEqual(sensor_msg["mac"], "582D345265BF")
+        self.assertEqual(sensor_msg["packet"], "no packet id")
+        self.assertTrue(sensor_msg["data"])
+        self.assertEqual(sensor_msg["temperature"], 24.1)
+        self.assertEqual(sensor_msg["humidity"], 42.9)
+        self.assertEqual(sensor_msg["battery"], 37)
+        self.assertEqual(sensor_msg["rssi"], -47)
+
+    def test_qingping_CGG1(self):
+        """Test Qingping parser for CGG1."""
+        data_string = "043E2402010000B24410342D58180201061416CDFD0807B24410342D580104CA004502020138AF"
+        data = bytes(bytearray.fromhex(data_string))
+        sensor_msg, tracker_msg = ble_parser(self, data)
+
+        self.assertEqual(sensor_msg["firmware"], "Qingping")
+        self.assertEqual(sensor_msg["type"], "CGG1")
+        self.assertEqual(sensor_msg["mac"], "582D341044B2")
+        self.assertEqual(sensor_msg["packet"], "no packet id")
+        self.assertTrue(sensor_msg["data"])
+        self.assertEqual(sensor_msg["temperature"], 20.2)
+        self.assertEqual(sensor_msg["humidity"], 58.1)
+        self.assertEqual(sensor_msg["battery"], 56)
+        self.assertEqual(sensor_msg["rssi"], -81)
+
+    def test_qingping_no_data(self):
+        """Test Qingping parser with message with no data."""
+        data_string = "043E1F020100000CA4288CCF04130201060B16CDFD080E0AA4288CCF048CCF0481CB"
+        data = bytes(bytearray.fromhex(data_string))
+        sensor_msg, tracker_msg = ble_parser(self, data)
+
+        self.assertEqual(sensor_msg, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
