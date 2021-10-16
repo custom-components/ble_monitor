ca4baa4ec137446316575f957b1e1c5d4bf337a8
Ernst79 <e.klamer@gmail.com>
2021-10-16 11:09:06 +0200

    Add test to BlueMaestro parser

---
+++ custom_components/ble_monitor/test/test_bluemaestro.py
@@ -0,0 +1,23 @@
+"""The tests for the BlueMaestro ble_parser."""
+from ble_monitor.ble_parser import BleParser
+
+
+class TestBlueMaestro:
+    def test_BlueMaestro_THD(self):
+        """Test BlueMaestro Tempo Disc THD parser."""
+        data_string = "043e4702010400aabb611d12e03b11ff330117550e10061eff2f02a6ff0301000908111111111111111100000000330174536166650000000000000000616974536166650000000000a6"
+        data = bytes(bytearray.fromhex(data_string))
+        # pylint: disable=unused-variable
+        ble_parser = BleParser()
+        sensor_msg, tracker_msg = ble_parser.parse_data(data)
+
+        assert sensor_msg["firmware"] == "BlueMaestro"
+        assert sensor_msg["type"] == "Tempo Disc THD"
+        assert sensor_msg["mac"] == "E0121D61BBAA"
+        assert sensor_msg["packet"] == 1566
+        assert sensor_msg["data"]
+        assert sensor_msg["temperature"] == -20.9
+        assert sensor_msg["humidity"] == 67.8
+        assert sensor_msg['dewpoint'] == -25.3
+        assert sensor_msg["battery"] == 85
+        assert sensor_msg["rssi"] == -90
--- docs/_devices/Tempo Disc THD.md
+++ docs/_devices/Tempo Disc THD.md
@@ -2,7 +2,7 @@
 manufacturer: BlueMaestro
 name: Tempo Disc Bluetooth Temperature, Humidity and Dew Point Sensor Beacon and Data Logger
 model: Tempo Disc THD
-image: 
+image: BlueMaestro-tempo-disc-THD.jpg
 physical_description: Round sensor, no screen
 broadcasted_properties:
   - temperature
@@ -13,7 +13,7 @@ broadcasted_property_notes:
   - property: rssi
     note: >
       The RSSI sensor is disabled by default. You can enable the RSSI sensor by going to `configuration`, `integrations`, select `devices` on the BLE monitor integration tile and select your device. Click on the `+1 disabled entity` to show the disabled sensor and select the disabled entity. Finally, click on `Enable entity` to enable it. 
-broadcast_rate:
+broadcast_rate: Variable, at least once a minute.
 active_scan:
 encryption_key:
 custom_firmware:
