661043c05657683f8c1147a78bdaf2272b40a17c
Dmitry Mamontov <dm.mamontov@x5.ru>
2022-01-30 15:50:42 +0300

    Fix major and minor detection

--- custom_components/ble_monitor/ble_parser/altbeacon.py
+++ custom_components/ble_monitor/ble_parser/altbeacon.py
@@ -26,11 +26,10 @@ _LOGGER = logging.getLogger(__name__)
 
 DEVICE_TYPE: Final = "AltBeacon"
 
-
 def parse_altbeacon(self, data: str, comp_id: int, source_mac: str, rssi: float):
     if len(data) >= 27:
         uuid = data[6:22]
-        (major, minor, power) = unpack(">hhb", data[22:27])
+        (major, minor, power) = unpack(">HHb", data[22:27])
 
         tracker_data = {
             CONF_RSSI: rssi,
