2fa1c7524fa3ab393e846649bd9ff1a4e952bb29
Radek Svoboda <radek.svoboda@gmail.com>
2022-02-01 20:52:07 +0100

    Parse temperature and humidity, too

--- custom_components/ble_monitor/ble_parser/sensirion.py
+++ custom_components/ble_monitor/ble_parser/sensirion.py
@@ -11,10 +11,13 @@ def parse_sensirion(self, data, source_mac, rssi):
 
     if msg_length == 14 and device_id == 0x06:
         """Sensirion CO2 gadget"""
-        (unk1, unk2, unk3, unk4, co2) = unpack("<HHHHH", xvalue)
+        # based on https://github.com/sparkfun/SparkFun_SCD4x_Arduino_Library/blob/main/src/SparkFun_SCD4x_Arduino_Library.cpp#L239-L242
+        (unk1, unk2, temperature, humidity, co2) = unpack("<HHHHH", xvalue)
 
         result.update({
-            "carbon dioxide": co2
+            "carbon_dioxide": float(co2),
+            "humidity": humidity*100/65536.0,
+            "temperature": -45 + temperature*175/65536.0
         })
 
     packet_id = xvalue.hex()
