7fc90022e2e64d2f2f9ef6b34c5d240b3fc7d871
Ernst Klamer <e.klamer@gmail.com>
2023-06-03 20:09:54 +0200

    Fix tests

--- custom_components/ble_monitor/ble_parser/holyiot.py
+++ custom_components/ble_monitor/ble_parser/holyiot.py
@@ -34,7 +34,7 @@ def parse_holyiot(self, data, source_mac, rssi):
             meas_value = data[15] + data[16] / 100
         elif meas_type == 2:
             measurement_type = "pressure"
-            value = unpack(">h", data[15:17])
+            value, = unpack(">h", data[15:17])
             meas_value = 8000 + value
         elif meas_type == 3:
             measurement_type = "humidity"
