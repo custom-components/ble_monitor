5ba6a994de7ebcd448404f577b9c9f97ae1cd49a
Johan Björnstedt <johan.bjornstedt@acconeer.com>
2023-12-28 15:18:42 +0100

    Add support for Acconeer XM126 Distance

--- custom_components/ble_monitor/test/test_acconeer_parser.py
+++ custom_components/ble_monitor/test/test_acconeer_parser.py
@@ -44,3 +44,24 @@ class TestAcconeer:
         assert sensor_msg["temperature"] == 22
         assert sensor_msg["motion"] == 1
         assert sensor_msg["rssi"] == -62
+
+
+    def test_acconeer_xm126_distance(self):
+        """Test acconeer parser for Acconeer XM126 Distance."""
+        data_string = "043e22020103013412b69009e01602010612ffc0ac906400160058050000000000000000c2"
+
+        data = bytes(bytearray.fromhex(data_string))
+
+        # pylint: disable=unused-variable
+        ble_parser = BleParser()
+        sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
+
+        assert sensor_msg["firmware"] == "Acconeer"
+        assert sensor_msg["type"] == "Acconeer XM126 Distance"
+        assert sensor_msg["mac"] == "E00990B61234"
+        assert sensor_msg["packet"] == "6400160058050000000000000000"
+        assert sensor_msg["data"]
+        assert sensor_msg["battery"] == 100
+        assert sensor_msg["temperature"] == 22
+        assert sensor_msg["distance_mm"] == 1368
+        assert sensor_msg["rssi"] == -62
