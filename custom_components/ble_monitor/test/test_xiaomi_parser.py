36595353e23314214f9b5b1b6cd691a1362e86c2
DesertOvercrowded <desertovercrowded@gmail.com>
2026-09-10 08:38:19 +0200

    fix: isort issues

--- custom_components/ble_monitor/test/test_xiaomi_parser.py
+++ custom_components/ble_monitor/test/test_xiaomi_parser.py
@@ -2,8 +2,8 @@
 import datetime
 
 from ble_monitor.ble_parser import BleParser
-from ble_monitor.ble_parser.xiaomi import (obj0010, obj4e0c, obj4e0d, obj4e0e,
-                                           obj4e16, obj4e17, obj5a16, obj560c,
+from ble_monitor.ble_parser.xiaomi import (obj4e0c, obj4e0d, obj4e0e, obj4e16,
+                                           obj4e17, obj5a16, obj0010, obj560c,
                                            obj560d, obj560e, obj1001, obj3003,
                                            obj4810, obj4850, obj4851, obj4852,
                                            obj5010)
